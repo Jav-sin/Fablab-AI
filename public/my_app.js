f53f456680ea23d9e5ff8337059a43eeb400561c
Jav-sin <jsavila@miners.utep.edu>
2024-07-15 15:34:55 -0600

    Arith and console

--- public/my_app.js
+++ public/my_app.js
@@ -1,3 +1,21 @@
 function alertMessage() {
     alert('hello, world');
 }
+
+var x = 3;
+console.log (x)
+
+const name = "Jonathan"
+console.log(name)
+
+console.log(x + name)
+console.log(output)
+
+var y = 5
+var output2 = x + y
+console.log(output2)
+
+if (y > x) {
+    console.log("X is greater than Y")
+for (let )
+}
